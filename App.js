684cf3be8aab551cdb12470d0d611b2b20ba57c7
apiyo <franklineapiyo@gmail.com>
2022-12-31 10:11:19 +0300

    Intial commit: create StickerSmash and add a placeholder image

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const placeholderImage = require('./assets/image/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={placeholderImage} style={styles.image} />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18
+  },
+});
