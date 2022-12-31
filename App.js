af06d07518b3b90fcae4ec4ffbdb694bcacd9d47
apiyo <franklineapiyo@gmail.com>
2022-12-31 11:11:29 +0300

    Add a way to pick images from gallery

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+
+import * as ImagePicker from 'expo-image-picker';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -7,14 +10,31 @@ import ImageViewer from './components/ImageViewer';
 const placeholderImage = require('./assets/image/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowEditing: true,
+      quality: 1
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={placeholderImage}/>
+        <ImageViewer
+          placeholderImageSource={placeholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo"/>
-        <Button label="Use this photo"/>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,14 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export default function Button({ label, theme }) {
+export default function Button({ label, theme, onPress }) {
     if (theme === 'primary') {
         return (
             <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}
             >
                 <Pressable
                     style={[styles.button, { backgroundColor: "#fff" }]}
-                    onPress={() => alert('You pressed a button.')}
+                    onPress={onPress}
                 >
                     <FontAwesome
                         name="picture-o"
@@ -23,7 +23,7 @@ export default function Button({ label, theme }) {
     }
     return (
         <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+            <Pressable style={styles.button} onPress={() => alert('You have chosen to use the default photo.')}>
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
